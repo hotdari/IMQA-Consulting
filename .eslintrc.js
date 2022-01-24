abf1a4474dd13666381c570147c5cbd5814af95c
hotdari <hotdari90@gmail.com>
2022-01-24 09:14:44 +0900

    <Docs>:eslint 내용 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,70 +1,3 @@
-# IMQA-Consulting
-
-IMQA 제품을 컨설팅하는데 도움을 드립니다.
-
-# Version
-
-- node : v14.18.3 (nvm 사용)
-- vue : @vue/cli 4.5.15
-
-# Setting
-
-## nvm node version install
-```shell
-// 설치된 버전들 확인
-nvm list
-
-// 14 버전 설치
-nvm install 14
-
-// 14버전 사용 및 기본 버전변경
-nvm use default 14
-
-// 14버전 강제로 변경
-nvm alias default 14
-```
-
-## vue cli 설치
-```shell
-// 설치된 버전 확인
-vue --version
-
-// vue cli 설치
-npm i -g @vue/cli
-```
-
-# Develop
-
-```shell
- npm run electron:serve
-```
-
-# Deploy
-
-```shell
-dist_eletron 폴더에 파일이 생성됩니다.
-```
-
-## Window
-
-```shell
-// window 배포
-npm run electron:build-win
-
-// window 32, 64비트 배포
-npm run electron:build-win32
-npm run electron:build-win64
-```
-
-## Mac
-
-```shell
-// mac 배포
-npm run electron:build-mac
-```
-
-# .eslintrc.js
-```javascript
 module.exports = {
   root: true,
   env: {
@@ -77,11 +10,6 @@ module.exports = {
     SharedArrayBuffer: "readonly"
   },
   parser: "vue-eslint-parser",
-  parserOptions: {
-    parser: "babel-eslint",
-    sourceType: "module",
-    ecmaVersion: 12
-  },
   rules: {
     "arrow-parens": ["error", "as-needed"],
     "comma-spacing": ["error", { before: false, after: true }],
@@ -143,7 +71,6 @@ module.exports = {
     "vue/html-self-closing": "off",
     "vue/prop-name-casing": "off",
     "vue/require-prop-types": "off",
-    "vue/require-prop-types": "off",
     "vue/order-in-components": [
       //컴포넌트 order 순서
       "error",
@@ -178,5 +105,3 @@ module.exports = {
     parser: 'babel-eslint'
   }
 };
-
-```
