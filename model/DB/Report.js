05ce10d455178bdc1451526221bc1df6be12f8b3
seol1214 <>
2022-02-10 18:59:02 +0900

    <Fix>: 주석 위치 수정

--- model/DB/Report.js
+++ model/DB/Report.js
@@ -10,24 +10,25 @@ class Report {
     this.dao = dao;
   }
 
+  /**
+   * 테이블 생성
+   * report_id : 레포트 ID
+   * project_id : 컨설팅 ID (project.project_id FK)
+   * desc : 설명
+   * content : 컨설팅 내용 (String -> JSON Array로 변환 필요)
+   */
   createTable(){
-    /**
-     * 테이블 생성
-     * report_id : 레포트 ID
-     * project_id : 컨설팅 ID (project.project_id FK)
-     * desc : 설명
-     * content : 컨설팅 내용 (String -> JSON Array로 변환 필요)
-     */
     this.dao.run('CREATE TABLE IF NOT EXISTS report ( report_id INTEGER PRIMARY KEY AUTOINCREMENT, FOREIGN KEY(project_id) REFERENCES project(project_id), desc TEXT, content TEXT )',[], arg => {
       console.log('create Report :: ', arg);
       return arg
     });
   }
+
+  /**
+   * 컨설팅 ID로 레포트 정보 가져오기
+   * @param project_id  Number 컨설팅 ID
+   */
   selectReport(arg){
-    /**
-     * 컨설팅 ID로 레포트 정보 가져오기
-     * @param project_id  Number 컨설팅 ID
-     */
     return new Promise((resolve, reject) => {
       this.dao.get(`SELECT * FROM report WHERE project_id = ${arg.project_id}`, (err, rows) => {
         console.log(rows);
@@ -36,20 +37,20 @@ class Report {
       });
     })
   }
+
+  /**
+   * 레포트 정보 저장
+   * @param report_id Number 레포트 ID
+   * @param project_id  Number 컨설팅 ID
+   * @param desc  String 설명
+   * @param content Object 컨설팅 내용 (JSON Array)
+   */
   insertReport(arg){
-    /**
-     * 레포트 정보 저장
-     * @param report_id Number 레포트 ID
-     * @param project_id  Number 컨설팅 ID
-     * @param desc  String 설명
-     * @param content Object 컨설팅 내용 (JSON Array)
-     */
     return this.dao.run(`INSERT INTO report VALUES(${arg.report_id}, ${arg.project_id}, ${arg.desc}, ${JSON.stringify(arg.content)})`,[], (err,arg)=>{});
     // 샘플
     // return this.dao.run('INSERT INTO report VALUES(?,?,?,?)',[1, 1, "설명", JSON.stringify(sample)], (err,arg)=>{});
   }
 }
-
 
 const report = new Report(db);
 export default report;
