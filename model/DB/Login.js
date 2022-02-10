89cc997e56b4585aa45eeb019fad918a184dd0dc
seol1214 <>
2022-02-10 18:32:49 +0900

    <Fix>: login > 주석 추가

--- model/DB/Login.js
+++ model/DB/Login.js
@@ -1,6 +1,14 @@
-// 로그인 (실제 로그인 처리는 Live서버에서 처리하고 여기는 id(이메일)와 쿠키값 저장/관리)
+/**
+ * 로그인 (실제 로그인 처리는 Live서버에서 처리하고 여기는 id(이메일)와 쿠키값 저장/관리)
+ *
+ * Live 로그인해서 쿠키(토큰)값을 가져옴
+ * 쿠키가 있을 때 : selectLogin id로 조회되는 데이터가 있는지 확인
+ *               데이터가 있으면 updateLogin (last_login 갱신)
+ *               데이터가 없으면 insertLogin
+ * 쿠키가 없을 때 : 화면에서 로그인 재시도 메시지 안내
+ */
 
-import {db} from "../../db";
+import db from "../../db/db";
 
 class Login {
   constructor(dao) {
@@ -8,12 +16,22 @@ class Login {
   }
 
   createTable(){
+    /**
+     * 테이블 생성
+     * id : 라이브 서버 사용자 ID (email)
+     * token : 라이브 서버 로그인 토큰
+     * last_login : 마지막 로그인 시간
+     */
     // this.dao.run('CREATE TABLE IF NOT EXISTS login (id TEXT PRIMARY KEY, password TEXT, token TEXT, last_login TEXT)',[], arg=>{
     this.dao.run('CREATE TABLE IF NOT EXISTS login (id TEXT PRIMARY KEY, token TEXT, last_login TEXT)',[], arg=>{
       console.log('create Login :: ', arg);
     });
   }
   selectLogin(arg){
+    /**
+     * 컨설팅 ID로 레포트 정보 가져오기
+     * @param id  Number 라이브 서버 사용자 ID
+     */
     return new Promise((resolve, reject) => {
       this.dao.get(`SELECT * FROM login WHERE id=${arg.id}`, (err, rows) => {
         if(err){return reject(err);}
@@ -22,18 +40,21 @@ class Login {
     })
   }
   insertLogin(arg){
+    /**
+     * 라이브 정보 insert
+     * @param id  String 라이브 서버 사용자 ID
+     * @param token  String 토큰
+     */
     return this.dao.run('INSERT INTO login VALUES(${arg.id}, ${arg.token}, datetime(now))', (err,arg)=>{});
   }
   updateLogin(arg){
+    /**
+     * 라이브 정보 update
+     * @param id  String 라이브 서버 사용자 ID
+     */
     return this.dao.run(`UPDATE login SET last_login=datetime(now) WHERE id=${arg.id}`, (err,arg)=>{});
   }
 }
-
-// Live 로그인해서 쿠키(토큰)값을 가져옴
-// 쿠키가 있을 때 : selectLogin id로 조회되는 데이터가 있는지 확인
-//               데이터가 있으면 updateLogin (last_login 갱신)
-//               데이터가 없으면 insertLogin
-// 쿠키가 없을 때 : 화면에서 로그인 재시도 메시지 안내
 
 const login = new Login(db);
 export default login;
